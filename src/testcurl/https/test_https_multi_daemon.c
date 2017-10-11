c6f8b3415525c9f4304c71fd4cc9fbb4430be085
ghaderer <ghaderer@wyplay.com>
2017-10-11 20:41:19 +0200

    test_https_multi_daemon: test all TLS engines.

--- src/testcurl/https/test_https_multi_daemon.c
+++ src/testcurl/https/test_https_multi_daemon.c
@@ -42,8 +42,9 @@ extern const char srv_self_signed_cert_pem[];
  */
 static int
 test_concurent_daemon_pair (void *cls, 
-			    const char *cipher_suite,
-                            int proto_version)
+                            const char *cipher_suite,
+                            int proto_version,
+                            enum MHD_TLS_EngineType tls_engine_type)
 {
 
   int ret;
@@ -53,6 +54,7 @@ test_concurent_daemon_pair (void *cls,
   d1 = MHD_start_daemon (MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_TLS |
                          MHD_USE_ERROR_LOG, DEAMON_TEST_PORT,
                          NULL, NULL, &http_ahc, NULL,
+                         MHD_OPTION_TLS_ENGINE_TYPE, tls_engine_type,
                          MHD_OPTION_HTTPS_MEM_KEY, srv_key_pem,
                          MHD_OPTION_HTTPS_MEM_CERT, srv_self_signed_cert_pem,
                          MHD_OPTION_END);
@@ -66,6 +68,7 @@ test_concurent_daemon_pair (void *cls,
   d2 = MHD_start_daemon (MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_TLS |
                          MHD_USE_ERROR_LOG, DEAMON_TEST_PORT + 1,
                          NULL, NULL, &http_ahc, NULL,
+                         MHD_OPTION_TLS_ENGINE_TYPE, tls_engine_type,
                          MHD_OPTION_HTTPS_MEM_KEY, srv_key_pem,
                          MHD_OPTION_HTTPS_MEM_CERT, srv_self_signed_cert_pem,
                          MHD_OPTION_END);
@@ -95,6 +98,9 @@ int
 main (int argc, char *const *argv)
 {
   unsigned int errorCount = 0;
+  int tls_engine_index;
+  enum MHD_TLS_EngineType tls_engine_type;
+  const char *tls_engine_name;
   FILE *cert;
   const char *aes256_sha = "AES256-SHA";
 
@@ -126,8 +132,14 @@ main (int argc, char *const *argv)
       aes256_sha = "rsa_aes_256_sha";
     }
 
-  errorCount +=
-    test_concurent_daemon_pair (NULL, aes256_sha, CURL_SSLVERSION_TLSv1);
+  tls_engine_index = 0;
+  while (0 <= (tls_engine_index = iterate_over_available_tls_engines (tls_engine_index,
+                                                                      &tls_engine_type,
+                                                                      &tls_engine_name)))
+    {
+      errorCount +=
+        test_concurent_daemon_pair (NULL, aes256_sha, CURL_SSLVERSION_TLSv1, tls_engine_type);
+    }
 
   print_test_result (errorCount, "concurent_daemon_pair");
 
